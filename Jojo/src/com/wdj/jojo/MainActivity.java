0a14b3b9166f7d36bc704bfc21338add23be4037
LostTemple <wdjxysc@163.com>
2015-01-27 18:31:09 +0800

    20150127183000

--- Jojo/src/com/wdj/jojo/MainActivity.java
+++ Jojo/src/com/wdj/jojo/MainActivity.java
@@ -4,25 +4,28 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	
+	private TextView textView;
+	private Button button;
+	private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        
-        TextView textView = new TextView(null);
-        
         return true;
     }
 
